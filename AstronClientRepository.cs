c0786f5c566b0df1b50ae7096ab22229adcf542f
bobbybee <j104@rocketmail.com>
2014-06-27 11:12:25 -0400

    Initial commit

---
+++ AstronClientRepository.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * AstronClientRepository.cs
+ * Copyright (C) bobbybee, shadowcoder, and the Astron team 2014
+ * C# implementation of the Astron multiplayer client
+ * See https://github.com/Astron/Astron for more information
+ * */
+
+public class AstronClientRepository {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
