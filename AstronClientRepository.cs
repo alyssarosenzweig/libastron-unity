a6d7b1a2a51d7cdf279c531fd5c1f81dc8de2dd8
bobbybee <j104@rocketmail.com>
2014-08-15 17:49:30 -0400

    sendClientHello; datagram creation wrapper begun

--- AstronClientRepository.cs
+++ AstronClientRepository.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Net;
 using System.Net.Sockets;
+using System.IO;
 
 /*
  * AstronClientRepository.cs
@@ -9,11 +11,65 @@ using System.Net.Sockets;
  * See https://github.com/Astron/Astron for more information
  * */
 
+enum MessageTypes : ushort {
+	CLIENT_HELLO = 1,
+	CLIENT_HELLO_RESP = 2,
+	CLIENT_DISCONNECT = 3,
+	CLIENT_EJECT = 4,
+	CLIENT_HEARTBEAT = 5,
+	CLIENT_OBJECT_SET_FIELD = 120,
+	CLIENT_OBJECT_SET_FIELDS = 121,
+	CLIENT_OBJECT_LEAVING = 132,
+	CLIENT_OBJECT_LEAVING_OWNER = 161,
+	CLIENT_OBJECT_LOCATION = 140,
+	CLIENT_ENTER_OBJECT_REQUIRED = 142,
+	CLIENT_ENTER_OBJECT_REQUIRED_OTHER = 143,
+	CLIENT_ENTER_OBJECT_REQUIRED_OWNER = 172,
+	CLIENT_ENTER_OBJECT_REQUIRED_OTHER_OWNER = 173,
+	CLIENT_DONE_INTEREST_RESP = 204,
+	CLIENT_ADD_INTEREST = 200,
+	CLIENT_ADD_INTEREST_MULTIPLE = 201,
+	CLIENT_REMOVE_INTEREST = 203
+};
+
+public class AstronStream : MemoryStream {
+	public void Flush(BinaryWriter o) {
+		o.Write( (UInt16) Length);
+
+		byte[] payload = ToArray();
+
+		o.Write(payload);
+		Seek (0, SeekOrigin.Begin);
+		SetLength (0);
+	}
+
+}
+
+public class DatagramOut : BinaryWriter {
+	public DatagramOut(AstronStream s) : base(s) {
+
+	}
+
+	public override void Write(string s) {
+		Write( (UInt16) s.Length );
+
+		Write( s.ToCharArray() );
+	}
+}
+
+
+
+
 public class AstronClientRepository {
 	public bool m_connected = false;
 	private string m_dcFile;
 
 	private TcpClient socket;
+	private NetworkStream stream;
+	private BinaryWriter writer;
+	private DatagramOut odgram;
+
+	private AstronStream sout;
 
 	private void initWithDC(string dcFile) {
 		m_connected = false;
@@ -40,8 +96,22 @@ public class AstronClientRepository {
 
 		m_connected = true;
 
+		stream = socket.GetStream();
+		writer = new BinaryWriter(stream);
+
+		sout = new AstronStream();
+		odgram = new DatagramOut(sout);
+
 		return m_connected;
 	}
 
+
+
+	public void sendClientHello(string version, UInt32 dcHash) {
+		odgram.Write((UInt16) MessageTypes.CLIENT_HELLO);
+		odgram.Write(dcHash);
+		odgram.Write(version);
+		sout.Flush(writer);
+	}
 		
 }
